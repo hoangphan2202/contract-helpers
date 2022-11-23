ca0be301af41935fa1eb01a429268979df2b2131
hoangphan <vanhoang22021997@gmail.com>
2022-11-23 17:17:32 +0700

    init commit

---
+++ dist/cjs/index.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PERMISSION_MAP = exports.PERMISSION = void 0;
+const tslib_1 = require("tslib");
+tslib_1.__exportStar(require("./permissions-manager"), exports);
+var PermissionManagerTypes_1 = require("./permissions-manager/types/PermissionManagerTypes");
+Object.defineProperty(exports, "PERMISSION", { enumerable: true, get: function () { return PermissionManagerTypes_1.PERMISSION; } });
+Object.defineProperty(exports, "PERMISSION_MAP", { enumerable: true, get: function () { return PermissionManagerTypes_1.PERMISSION_MAP; } });
+tslib_1.__exportStar(require("./v3-UiIncentiveDataProvider-contract"), exports);
+tslib_1.__exportStar(require("./v3-UiPoolDataProvider-contract"), exports);
+tslib_1.__exportStar(require("./wallet-balance-provider"), exports);
+tslib_1.__exportStar(require("./cl-feed-registry"), exports);
+tslib_1.__exportStar(require("./uiStakeDataProvider-contract"), exports);
+// services
+tslib_1.__exportStar(require("./incentive-controller"), exports);
+tslib_1.__exportStar(require("./incentive-controller-v2"), exports);
+tslib_1.__exportStar(require("./erc20-contract"), exports);
+tslib_1.__exportStar(require("./lendingPool-contract"), exports);
+tslib_1.__exportStar(require("./faucet-contract"), exports);
+tslib_1.__exportStar(require("./staking-contract"), exports);
+tslib_1.__exportStar(require("./governance-contract"), exports);
+tslib_1.__exportStar(require("./governance-contract/types"), exports);
+tslib_1.__exportStar(require("./governance-power-delegation-contract"), exports);
+tslib_1.__exportStar(require("./v3-pool-contract"), exports);
+tslib_1.__exportStar(require("./synthetix-contract"), exports);
+tslib_1.__exportStar(require("./baseDebtToken-contract"), exports);
+// commons
+tslib_1.__exportStar(require("./commons/types"), exports);
+tslib_1.__exportStar(require("./commons/ipfs"), exports);
+tslib_1.__exportStar(require("./commons/utils"), exports);
+//# sourceMappingURL=index.js.map
---
+++ src/v3-pool-contract/lendingPoolTypes.ts
@@ -0,0 +1,162 @@
+import { SignatureLike } from '@ethersproject/bytes';
+import { BytesLike } from 'ethers';
+import {
+  tEthereumAddress,
+  InterestRate,
+  PermitSignature,
+} from '../commons/types';
+
+export type LPSupplyParamsType = {
+  user: tEthereumAddress;
+  reserve: tEthereumAddress;
+  amount: string;
+  onBehalfOf?: tEthereumAddress;
+  referralCode?: string;
+  useOptimizedPath?: boolean;
+};
+
+export type LPWithdrawParamsType = {
+  user: tEthereumAddress;
+  reserve: tEthereumAddress;
+  amount: string;
+  onBehalfOf?: tEthereumAddress;
+  aTokenAddress?: tEthereumAddress;
+  useOptimizedPath?: boolean;
+};
+
+export type LPBorrowParamsType = {
+  user: tEthereumAddress;
+  reserve: tEthereumAddress;
+  amount: string;
+  interestRateMode: InterestRate;
+  debtTokenAddress?: tEthereumAddress;
+  onBehalfOf?: tEthereumAddress;
+  referralCode?: string;
+  useOptimizedPath?: boolean;
+};
+
+export type LPRepayParamsType = {
+  user: tEthereumAddress;
+  reserve: tEthereumAddress;
+  amount: string;
+  interestRateMode: InterestRate;
+  onBehalfOf?: tEthereumAddress;
+  useOptimizedPath?: boolean;
+};
+
+export type LPSwapBorrowRateMode = {
+  user: tEthereumAddress;
+  reserve: tEthereumAddress;
+  interestRateMode: InterestRate;
+  useOptimizedPath?: boolean;
+};
+
+export type LPSetUsageAsCollateral = {
+  user: tEthereumAddress;
+  reserve: tEthereumAddress;
+  usageAsCollateral: boolean;
+  useOptimizedPath?: boolean;
+};
+export type LPLiquidationCall = {
+  liquidator: tEthereumAddress;
+  liquidatedUser: tEthereumAddress;
+  debtReserve: tEthereumAddress;
+  collateralReserve: tEthereumAddress;
+  purchaseAmount: string;
+  getAToken?: boolean;
+  liquidateAll?: boolean;
+  useOptimizedPath?: boolean;
+};
+
+export type LPSwapCollateral = {
+  user: tEthereumAddress;
+  flash?: boolean;
+  fromAsset: tEthereumAddress; // List of addresses of the underlying asset to be swap from
+  fromAToken: tEthereumAddress;
+  toAsset: tEthereumAddress; // List of the addresses of the reserve to be swapped to and deposited
+  fromAmount: string; // List of amounts to be swapped. If the amount exceeds the balance, the total balance is used for the swap
+  minToAmount: string;
+  permitSignature?: PermitSignature;
+  swapAll: boolean;
+  referralCode?: string;
+  augustus: tEthereumAddress;
+  swapCallData: BytesLike;
+};
+
+export type LPParaswapRepayWithCollateral = {
+  user: tEthereumAddress;
+  fromAsset: tEthereumAddress;
+  fromAToken: tEthereumAddress;
+  assetToRepay: tEthereumAddress; // List of addresses of the underlying asset to be swap from
+  repayWithAmount: string;
+  repayAmount: string; // List of amounts to be swapped. If the amount exceeds the balance, the total balance is used for the swap
+  permitSignature?: PermitSignature;
+  repayAllDebt?: boolean;
+  rateMode: InterestRate;
+  referralCode?: string;
+  flash?: boolean;
+  swapAndRepayCallData: BytesLike;
+  augustus: tEthereumAddress;
+};
+
+export type LPFlashLoan = {
+  user: tEthereumAddress;
+  receiver: tEthereumAddress;
+  assets: tEthereumAddress[];
+  amounts: string[];
+  modes: InterestRate[];
+  onBehalfOf?: tEthereumAddress;
+  referralCode?: string;
+};
+
+export type LPFlashLiquidation = {
+  user: tEthereumAddress;
+  collateralAsset: tEthereumAddress;
+  borrowedAsset: tEthereumAddress;
+  debtTokenCover: string;
+  liquidateAll: boolean;
+  initiator: tEthereumAddress;
+  useEthPath?: boolean;
+};
+
+export type LPSupplyWithPermitType = {
+  user: tEthereumAddress;
+  reserve: tEthereumAddress;
+  onBehalfOf?: tEthereumAddress;
+  amount: string;
+  signature: SignatureLike;
+  referralCode?: string;
+  useOptimizedPath?: boolean;
+  deadline: string;
+};
+
+export type LPRepayWithPermitParamsType = {
+  user: tEthereumAddress;
+  reserve: tEthereumAddress;
+  amount: string;
+  interestRateMode: InterestRate;
+  onBehalfOf?: tEthereumAddress;
+  signature: SignatureLike;
+  useOptimizedPath?: boolean;
+  deadline: string;
+};
+
+export type LPSignERC20ApprovalType = {
+  user: tEthereumAddress;
+  reserve: tEthereumAddress;
+  amount: string;
+  deadline: string;
+};
+
+export type LPSetUserEModeType = {
+  user: string;
+  categoryId: number;
+};
+
+export type LPRepayWithATokensType = {
+  user: string;
+  reserve: string;
+  amount: string;
+  rateMode: InterestRate;
+  useOptimizedPath?: boolean;
+};
