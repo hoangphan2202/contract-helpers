c78af0345d1d50afa40b8ae1c2cc125b5c3923d3
hoangphan <vanhoang22021997@gmail.com>
2022-11-25 16:54:44 +0700

    change abi

--- dist/esm/ui-pool-data-provider/typechain/UiPoolDataProviderFactory.d.ts
+++ dist/esm/ui-pool-data-provider/typechain/UiPoolDataProviderFactory.d.ts
@@ -3,7 +3,31 @@ import { Provider } from '@ethersproject/providers';
 import { ContractFactory } from '@ethersproject/contracts';
 import { UiPoolDataProvider } from './UiPoolDataProvider';
 export declare class UiPoolDataProviderFactory extends ContractFactory {
-    constructor(signer?: Signer);
+    static readonly abi: {
+        inputs: {
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        name: string;
+        outputs: ({
+            components: {
+                internalType: string;
+                name: string;
+                type: string;
+            }[];
+            internalType: string;
+            name: string;
+            type: string;
+        } | {
+            internalType: string;
+            name: string;
+            type: string;
+            components?: undefined;
+        })[];
+        stateMutability: string;
+        type: string;
+    }[];
     static connect(address: string, signerOrProvider: Signer | Provider): UiPoolDataProvider;
 }
 //# sourceMappingURL=UiPoolDataProviderFactory.d.ts.map
