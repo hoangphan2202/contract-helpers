1ed4391ae328153a5fce95c6a00068068bbe9ddf
hoangphan <vanhoang22021997@gmail.com>
2022-11-25 18:04:45 +0700

    buid new abi

--- dist/cjs/ui-incentive-data-provider/typechain/UiIncentiveDataProviderFactory.d.ts
+++ dist/cjs/ui-incentive-data-provider/typechain/UiIncentiveDataProviderFactory.d.ts
@@ -3,7 +3,46 @@ import { Provider } from '@ethersproject/providers';
 import { ContractFactory } from '@ethersproject/contracts';
 import { UiIncentiveDataProvider } from './UiIncentiveDataProvider';
 export declare class UiIncentiveDataProviderFactory extends ContractFactory {
-    constructor(signer?: Signer);
+    static readonly abi: {
+        inputs: {
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        name: string;
+        outputs: {
+            components: ({
+                internalType: string;
+                name: string;
+                type: string;
+                components?: undefined;
+            } | {
+                components: ({
+                    internalType: string;
+                    name: string;
+                    type: string;
+                    components?: undefined;
+                } | {
+                    components: {
+                        internalType: string;
+                        name: string;
+                        type: string;
+                    }[];
+                    internalType: string;
+                    name: string;
+                    type: string;
+                })[];
+                internalType: string;
+                name: string;
+                type: string;
+            })[];
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        stateMutability: string;
+        type: string;
+    }[];
     static connect(address: string, signerOrProvider: Signer | Provider): UiIncentiveDataProvider;
 }
 //# sourceMappingURL=UiIncentiveDataProviderFactory.d.ts.map
